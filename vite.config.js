c2c5b8181b5f9585d4224eae9287fc99dee09442
zanniadevweb <zanni.a.dev.web@gmail.com>
2023-08-03 23:36:44 +0200

    ++

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/vuetify_vue_3_template/",
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
